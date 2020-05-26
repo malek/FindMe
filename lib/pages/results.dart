146681e5f0159161ef71106415a55e4994d5c0b3
Oussama Abderrahim <OussamaLgt@Gmail.com>
2020-05-26 16:16:27 +0100

    Add social media buttons

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -5,6 +5,59 @@ class Results extends StatefulWidget {
   _ResultsState createState() => _ResultsState();
 }
 
+List<Widget> getTextWidgets(List<String> strings) {
+  List<Widget> list = new List<Widget>();
+  for (var i = 0; i < strings.length; i++) {
+    String website = strings[i];
+    list.add(SocialMediaButton(website: website));
+  }
+  return list;
+}
+
+class SocialMediaButton extends StatelessWidget {
+  const SocialMediaButton({
+    Key key,
+    @required this.website,
+  }) : super(key: key);
+
+  final String website;
+
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      onPressed: () {},
+      textColor: Colors.white,
+      padding: const EdgeInsets.all(0.0),
+      elevation: 4.0,
+      child: Container(
+          width: 300,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            gradient: LinearGradient(
+              colors: <Color>[
+                Color(0xFF0984e3),
+                // Color(0xFF1976D2),
+                Color(0xFF74b9ff),
+              ],
+            ),
+          ),
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Icon(Icons.android, color: Colors.black),
+              SizedBox(width: 20),
+              Text("Follow on $website",
+                  textWidthBasis: TextWidthBasis.parent,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
+            ],
+          )),
+    );
+  }
+}
+
 class _ResultsState extends State<Results> {
   @override
   Widget build(BuildContext context) {
@@ -55,53 +108,23 @@ class _ResultsState extends State<Results> {
                 ),
               ),
             ),
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-              clipBehavior: Clip.antiAlias,
-              color: Colors.white,
-              elevation: 8.0,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    height: 200,
-                    child: Column(
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+                  height: 300,
+                  child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Card(
-                          elevation: 4.0,
-                          child: Text(
-                            "Follow on Facebook",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        Card(
-                          elevation: 4.0,
-                          child: Text(
-                            "Follow on Instagram",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        Card(
-                          elevation: 4.0,
-                          child: Text(
-                            "Follow on Twitter",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        Card(
-                          elevation: 4.0,
-                          child: Text(
-                            "Follow on LinkedIn",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
+                      mainAxisSize: MainAxisSize.max,
+                      children: getTextWidgets(
+                          ["Facebook", "Twitter", "Instagram", "Github"]),),
+                ),
+              ],
+            ),
           ],
         ));
   }
