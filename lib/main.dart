6744ce1eeb3147ca5051a34af5da114ca11fb60a
lenovo <benzergaamalek@gmail.com>
2020-05-26 02:15:27 +0200

    starting register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,80 @@
+import 'package:findme/pages/register.dart';
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'constants.dart';
+import 'widgets/rounded_button.dart';
 import 'pages/register.dart';
-import 'pages/results.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => Home(),
-        '/register': (context) => Register(),
-        '/results': (context) => Results(),
-        '/home': (context) => Home(),
-      },
-    ));
+      title: 'Find Me',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: Theme.of(context).textTheme.apply(
+              displayColor: kBlackColor,
+            ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/flower.jpg"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.display3,
+                children: [
+                  TextSpan(
+                      text: "Find",
+                      style: TextStyle(color: Colors.deepOrangeAccent)),
+                  TextSpan(
+                    text: "Me.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orangeAccent),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .6,
+              child: RoundedButton(
+                text: "register",
+                fontSize: 20,
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Register();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../constants.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -6,35 +7,168 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String urlLink;
+  String brand;
+
+  Widget _UrlField() {
+    return TextFormField(
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0),
+      decoration: InputDecoration(
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(25.0),
+          borderSide: new BorderSide(),
+        ),
+        hintText: 'ADD LINK',
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      keyboardType: TextInputType.url,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Url is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        urlLink = value;
+      },
+    );
+  }
+
+  Widget _BrandNameField() {
+    return TextFormField(
+      style: TextStyle(fontWeight: FontWeight.bold, letterSpacing: 2.0),
+      decoration: InputDecoration(
+        border: new OutlineInputBorder(
+          borderRadius: new BorderRadius.circular(1.0),
+          borderSide: new BorderSide(style: BorderStyle.solid),
+        ),
+        hintText: 'Brand Name',
+        hintStyle: TextStyle(
+          color: Colors.orangeAccent,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      keyboardType: TextInputType.text,
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Brand Name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (String value) {
+        brand = value;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'FindMe',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.pink,
-      ),
-      body: RaisedButton.icon(
-          color: Colors.blue,
-          onPressed: () {
-            Navigator.pushNamed(context, '/results');
-          },
-          icon: Icon(
-            Icons.add_to_home_screen,
-            color: Colors.white,
-          ),
-          label: Text(
-            'Done',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold),
-          )),
-    );
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/flowerFloue.jpg'),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              margin: EdgeInsets.all(24),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 60.0),
+                  Center(
+                    child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/uploadPicTest.jpg'),
+                      radius: 40.0,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Center(
+                      child: Container(width: 200.0, child: _BrandNameField())),
+                  // Divider(
+                  //   height: 60.0,
+                  //   color: Colors.grey[800],
+                  // ),
+                  SizedBox(height: 66),
+                  _UrlField(),
+                  SizedBox(height: 30),
+                  Row(
+                    children: <Widget>[
+                      //cancel button
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 125,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.symmetric(vertical: 4),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 15),
+                                blurRadius: 30,
+                                color: Color(0xFF666666).withOpacity(.11),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: kIconColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 120),
+                      //Add button
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 100,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.symmetric(vertical: 4),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 15),
+                                blurRadius: 30,
+                                color: Color(0xFF666666).withOpacity(.11),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            'Add',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: kIconColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 60.0,
+                    color: Colors.grey[800],
+                  ),
+                ],
+              ),
+            )));
   }
 }
